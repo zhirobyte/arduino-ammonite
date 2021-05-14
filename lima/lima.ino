ae058d95ecd15134a9e59543467b24a933585f56
Zhirobyte <faizhanafi90@gmail.com>
2021-05-14 22:50:19 +0700

    mencoba dengan dua led plus breadboard

--- lima/lima.ino
+++ lima/lima.ino
@@ -1,23 +1,15 @@
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#include <Servo.h> 
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
-
-
-// Declare the Servo pin 
-int servoPin = 3; 
-// Create a servo object 
-Servo Servo1; 
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-
   lcd.init();                      // initialize the lcd 
-  lcd.init();
+  // We need to attach the servo to the used pin number 
+
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(8,0);
@@ -27,31 +19,15 @@ void setup() {
   lcd.setCursor(2,2);
   lcd.print("ammonite is live");
   lcd.setCursor(4,3);
-  lcd.print("[13 may 2021]");
+  lcd.print("[14 may 2021]");
    
 }
 
 
 // the loop function runs over and over again forever
 void loop() {
-             digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1100);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);           // wait for a second
-}
-
-void setup() { 
-   // We need to attach the servo to the used pin number 
-   Servo1.attach(servoPin); 
-}
-void loop(){ 
-   // Make servo go to 0 degrees 
-   Servo1.write(0); 
-   delay(1000); 
-   // Make servo go to 90 degrees 
-   Servo1.write(90); 
-   delay(1000); 
-   // Make servo go to 180 degrees 
-   Servo1.write(180); 
-   delay(1000); 
+  delay(1100);                       // wait for a second
 }
