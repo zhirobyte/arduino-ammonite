6d50de2d6b2f876d631676bb516355a64580abd2
Zhirobyte <faizhanafi90@gmail.com>
2021-07-03 12:58:42 +0700

    las update today

--- testrcwl/sample1/sample1.ino
+++ testrcwl/sample1/sample1.ino
@@ -6,7 +6,7 @@
 int detectPin = 10;
 bool detect = false;
 int led = 13;
-int gerak = 0;
+int gerak = 900;
 
 //ini adalah lcd fom github zhirobyte
 
@@ -17,7 +17,7 @@ LiquidCrystal_I2C lcd(0x27,20,4);       // set the LCD address to 0x27 for a 16 
 
 void setup() {
  Serial.begin(115200);
-
+lcd.begin(20,4);
  pinMode (detectPin, INPUT);
  pinMode (led, OUTPUT);
 
@@ -26,9 +26,9 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   lcd.init();
-  lcd.setCursor(0,0);
-  lcd.print("Booting up");
-  lcd.print("...");
+  lcd.setCursor(0,1);
+  lcd.print("Booting up.");
+
 }
 
 void loop() {
@@ -41,16 +41,22 @@ void loop() {
    lcd.init();   
    lcd.backlight();   
    lcd.setCursor(0,0);                  
-   lcd.print("Jumlah data terkini!");
+   lcd.print("Total data record");
    
- for (int i=0; i<=500; i++){
+ for (int i=0; i<=999999; i++){
 
 
-  lcd.setCursor(0,2);            
+  lcd.setCursor(0,1);            
 
-  lcd.print("Object detect:");
+  lcd.print("Object RCLW micro");
   int gerak = i;
-  lcd.print("Found");
+  lcd.setCursor(0,2);
+  lcd.print("n :");
+  lcd.print(gerak);
+  lcd.setCursor(0,3);
+  lcd.print("test v1.1");
+
+  
              
 
   
@@ -63,7 +69,7 @@ void loop() {
  else {
  digitalWrite(led, LOW);
  lcd.backlight();
-  lcd.print("...");
+  lcd.print("..");
  }
  delay(3000);
 }
